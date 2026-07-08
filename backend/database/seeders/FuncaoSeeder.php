3f56010519c2a30885502381e37fe783c384fb1d
Anna Leite <anna.email.07@gmail.com>
2026-07-07 22:41:10 -0300

    chore: cria estrutura inicial dos seeders

---
+++ backend/database/seeders/FuncaoSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FuncaoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
